67a1ea9792e6d665dec7b598258cb17136776be2
Tanmay Singal <tanmaysingal@icloud.com>
2022-02-05 19:46:42 +0530

    trigger ga

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.,
       ),
       home: const MyHomePage(),
     );
